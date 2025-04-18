b934e92ef25d7dca18bd1424d7581302c8c09d53
Maicom <maicomsoftinformatica@gmail.com>
2025-04-18 14:42:34 -0300

    Encapsulando lógica dentro da função sync_estoque

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from sync import sync_estoque
 
@@ -8,14 +7,3 @@ app = FastAPI()
 def atualizar_estoque():
     resultado = sync_estoque()
     return {"status": "ok", "dados": resultado}
-=======
-from fastapi import FastAPI
-from sync import sync_estoque
-
-app = FastAPI()
-
-@app.get("/estoque")
-def atualizar_estoque():
-    sync_estoque()
-    return {"status": "ok", "mensagem": "Estoque atualizado no WooCommerce"}
->>>>>>> be237b2 (Adiciona arquivos da API: sync, bling, woocommerce)
