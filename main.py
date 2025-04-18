88f89b3db6175ed73ea0b111e219dfdd5e2e4817
Maicom <maicomsoftinformatica@gmail.com>
2025-04-18 15:15:26 -0300

    Atualiza sync.py e main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from fastapi import FastAPI
 from sync import sync_estoque
 
@@ -7,3 +8,14 @@ app = FastAPI()
 def atualizar_estoque():
     resultado = sync_estoque()
     return {"status": "ok", "dados": resultado}
+=======
+from fastapi import FastAPI
+from sync import sync_estoque
+
+app = FastAPI()
+
+@app.get("/estoque")
+def atualizar_estoque():
+    sync_estoque()
+    return {"status": "ok", "mensagem": "Estoque atualizado no WooCommerce"}
+>>>>>>> be237b2 (Adiciona arquivos da API: sync, bling, woocommerce)
